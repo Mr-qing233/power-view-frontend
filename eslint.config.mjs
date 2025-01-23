697910f64eb83a0fa9d8b2180e1cdb7ddad61df5
Gin <2638353426@qq.com>
2025-01-23 19:32:20 +0800

    feat: initial project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import exp from "constants";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +11,79 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "eslint-config-prettier"
+  ),
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    ignores: ['node_modules/**'],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      prettier: 'eslint-plugin-prettier',
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off', // 允许使用 any
+      '@typescript-eslint/ban-ts-comment': 'off', // 允许使用 @ts-ignore
+      '@typescript-eslint/no-non-null-assertion': 'off', // 允许使用非空断言
+      '@typescript-eslint/no-var-requires': 'off', // 允许使用 CommonJS 的写法
+      'no-console': [
+        'warn',
+        {
+          allow: ['warn', 'error'],
+        },
+      ],
+      'no-debugger': 'warn',
+      "import/order": [
+        "error",
+        {
+          //按照分组顺序进行排序
+          "groups": ["builtin", "external", "parent", "sibling", "index", "internal", "object", "type"],
+          //通过路径自定义分组
+          "pathGroups": [
+            {
+              "pattern": "react*",
+              "group": "builtin",
+              "position": "before"
+            },
+            {
+              "pattern": "@/components/**",
+              "group": "parent",
+              "position": "before"
+            },
+            {
+              "pattern": "@/utils/**",
+              "group": "parent",
+              "position": "after"
+            },
+            {
+              "pattern": "@/apis/**",
+              "group": "parent",
+              "position": "after"
+            }
+          ],
+          "pathGroupsExcludedImportTypes": ["react"],
+          "newlines-between": "always", //每个分组之间换行
+          //根据字母顺序对每个组内的顺序进行排序
+          "alphabetize": {
+            "order": "asc",
+            "caseInsensitive": true
+          }
+        }
+      ]
+    },
+  }
 ];
 
 export default eslintConfig;
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,32 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import bundleAnalyzer from '@next/bundle-analyzer';
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true', //当环境变量ANALYZE为true时开启
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
+  sassOptions: {
+    additionalData: '@import "@/styles/index.scss";',
+  },
+  compiler: {
+    styledComponents: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https', //图片资源的协议
+        hostname: 'www.test.com', //图片资源的域名
+      },
+    ],
+  },
+  reactStrictMode: false,
+  experimental: {
+    turbo: {
+      resolveExtensions: ['.mdx', '.tsx', '.ts', '.jsx', '.js', '.mjs', '.json'],
+    },
+  },
 };
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig);
